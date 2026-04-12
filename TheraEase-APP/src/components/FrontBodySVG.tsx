a38aaa935098436b60c4b2ac22e309c97bf1adb5
vo-huy-hoang <huyhoangdn2005@gmail.com>
2026-04-13 00:47:10 +0700

    refactor: update SVG path coordinates for body zones and decorative elements in front and back views

--- TheraEase-APP/src/components/FrontBodySVG.tsx
+++ TheraEase-APP/src/components/FrontBodySVG.tsx
@@ -25,19 +25,20 @@ export default function FrontBodySVG({
   };
 
   const ZONES_PATHS = {
-    [ZONES.NECK]: "M 85 90 L 115 90 L 115 110 Q 125 145 155 170 L 45 170 Q 75 145 85 110 Z",
+    [ZONES.NECK]: "M 85 90 L 115 90 L 115 110 L 85 110 Z",
     [ZONES.SHOULDER_RIGHT]: "M 85 110 Q 60 100 30 130 Q 20 150 15 170 L 45 170 Q 75 145 85 110 Z",
     [ZONES.SHOULDER_LEFT]: "M 115 110 Q 140 100 170 130 Q 180 150 185 170 L 155 170 Q 125 145 115 110 Z",
-    [ZONES.CHEST]: "M 45 170 L 155 170 Q 150 220 145 270 L 55 270 Q 50 220 45 170 Z",
-    [ZONES.ABDOMEN]: "M 55 270 L 145 270 Q 155 300 160 340 L 100 370 L 40 340 Q 45 300 55 270 Z"
+    [ZONES.CHEST]: "M 85 110 L 115 110 Q 125 145 155 170 Q 150 220 145 270 L 55 270 Q 50 220 45 170 Q 75 145 85 110 Z",
+    [ZONES.ABDOMEN]: "M 55 270 L 145 270 L 152 325 L 100 340 L 48 325 Z"
   };
 
   const NON_INTERACTIVE_PATHS = [
     "M 85 90 L 115 90 Q 130 85 130 50 A 30 30 0 0 0 70 50 Q 70 85 85 90 Z",
     "M 15 170 Q 10 215 0 260 L 30 260 Q 40 215 45 170 Z",
     "M 185 170 Q 190 215 200 260 L 170 260 Q 160 215 155 170 Z",
-    "M 40 340 L 40 410 L 95 410 L 95 367.5 Z",
-    "M 160 340 L 160 410 L 105 410 L 105 367.5 Z"
+    "M 40 340 L 40 460 L 95 460 L 95 367.5 Z",
+    "M 160 340 L 160 460 L 105 460 L 105 367.5 Z",
+    "M 48 325 L 100 340 L 152 325 L 160 340 L 100 370 L 40 340 Z" // Pelvis (Hông/Bẹn)
   ];
 
   const DECORATIVE_PATHS = [
@@ -53,8 +54,8 @@ export default function FrontBodySVG({
     // Ngực
     "M 60 180 Q 80 195 100 185 Q 120 195 140 180",
     
-    // Rốn
-    "M 98 260 A 2 2 0 1 0 102 260 A 2 2 0 1 0 98 260",
+    // Rốn (dời xuống bụng một chút)
+    "M 98 285 A 2 2 0 1 0 102 285 A 2 2 0 1 0 98 285",
 
     // Đường bẹn (Groin) - Xóa bớt path cũ vì path mới của Abdomen đã thể hiện chữ V rồi
     // Giữ lại 1 đường rãnh nhẹ cho giống đồ giải phẫu
